c7443fa69a73288908fd9e02c652693f9c488123
berkantbyr <147828059+berkantbyr@users.noreply.github.com>
2025-11-26 11:58:30 +0300

    Revert "güncelleme"
    This reverts commit ee71864e98e4f243cc1a7c98b204a2431d468a3f.

--- backend/server.js
+++ backend/server.js
@@ -29,28 +29,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (!origin || allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin || allowedOrigins[0]);
-    res.header('Vary', 'Origin');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET,POST,PUT,PATCH,DELETE,OPTIONS'
-    );
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Content-Type,Authorization'
-    );
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(204);
-    }
-  }
-  return next();
-});
-
 console.log('Allowed origins:', allowedOrigins);
 app.use(express.json());
 // Statik dosyaları servis et (yüklenen resimler için)
